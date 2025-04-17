f1d7c9a530045c6010d32e002267791ff346a84a
lkcina <lkcina6@gmail.com>
2025-04-17 19:32:19 -0400

    Refactor simple mapping functions into AutoMapper Profiles.

---
+++ growers_market.Server/Mappers/ImageMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using growers_market.Server.Dtos.Address;
+using growers_market.Server.Dtos.ImagePosition;
+using growers_market.Server.Models;
+
+namespace growers_market.Server.Mappers
+{
+    public class ImageMapperProfile : Profile
+    {
+        public ImageMapperProfile()
+        {
+            CreateMap<Image, ImageDto>();
+        }
+    }
+}
